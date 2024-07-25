bf74b13e0c992491e1d850cf651fd38d65518bd2
hassanfawazdipo <150442083+hassanfawazdipo@users.noreply.github.com>
2024-07-25 12:42:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Welcome to opulence codes');
+alert('Welcome to opulence codes');
+
+function icon(){
+    var icon = document.getElementById("nav");
+    icon.classList.toggle("active");
+}
